a976bbdda309f8f77ecfcbbd04d24864b0c12b72
heartblood91 <github@hidemyhome.ovh>
2022-04-06 10:55:05 +0200

    [update] remove old query (for testing)

--- services/back/routes/queries.ts
+++ services/back/routes/queries.ts
@@ -7,19 +7,12 @@ import {
   database,
 } from './update/index.ts'
 
-
 const addQueries = () => {
   router.setQuery('/update/database', async () => {
     await database.updateDatabase()
 
-
     return new Response('Compute is over!', valid_response_headers)
   })
-
-  // router.setQuery('/coucou', () => {
-  //   const body = JSON.stringify({ message: "NOT FOUND" })
-  //   return new Response(body, valid_json_response_headers)
-  // })
 }
 
 export default addQueries
