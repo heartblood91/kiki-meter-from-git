fb0fc3e748ec30a8b06fedae15b5a8a14c37e1af
heartblood91 <github@hidemyhome.ovh>
2022-04-13 10:21:40 +0200

    [update] add new query to read database

--- services/back/routes/queries.ts
+++ services/back/routes/queries.ts
@@ -13,6 +13,18 @@ const addQueries = () => {
 
     return new Response('Compute is over!', valid_response_headers)
   })
+
+  router.setQuery('/get/database', async () => {
+    const database = await Deno.readTextFile('/workdir/database/kikimeters.json')
+
+    return new Response(database, {
+      status: valid_response_headers.status,
+      headers: {
+        ...valid_response_headers.headers,
+        "content-type": "application/json; charset=utf-8",
+      }
+    })
+  })
 }
 
 export default addQueries
